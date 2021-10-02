6f8f192b9cb130ab30946d0b6a15e402ebeb433f
KimHeajin <goodcandy200@gmail.com>
2021-10-02 15:10:29 +0900

    프로그래머스-코딩테스트연습/완전탐색/100 ※48~56까지의 코드(max값을 구해 최고점을 받은 사용자 추출하는 알고리즘)는 검색 내용을 참조함

--- JavaSkillStudy/programmers/codeTest/MockTest.java
+++ JavaSkillStudy/programmers/codeTest/MockTest.java
@@ -1,6 +1,7 @@
 package codeTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -10,44 +11,72 @@ public class MockTest {
 
 	public static void main(String[] args) {
 		
-		int[] one = {1,3,2,4,2, 1, 4, 5, 2, 3, 1, 1, 1};
+		int[] one = {1,3,2,4,2};
 		
 		System.out.println(solution(one));
 	}
 	public static int[] solution(int[] answers) {
-        int[] answer = {};
         
-        Map<String, Integer> mathGiveup = new HashMap<>();
-        int[] one = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5, 2, 1};
+		Map<Integer, Integer> mathGiveup = new HashMap<>();
+		mathGiveup.put(1, 0);
+		mathGiveup.put(2, 0);
+		mathGiveup.put(3, 0);
+
+		int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        mathGiveup.put("one", 0);
-        mathGiveup.put("two", 0);
-        mathGiveup.put("three", 0);
+        List<Integer> list =  new ArrayList<Integer>();
         
         int count = 0;
         int vlu = 0;
         for(int ans : answers) {
-        	if(ans == one[count]) {
-        		vlu = mathGiveup.get("one");
-        		mathGiveup.put("one", ++vlu);
-        	}if(ans == two[count]) {
-        		vlu = mathGiveup.get("two");
-        		mathGiveup.put("two", ++vlu);
-        	}if(ans == three[count]) {
-        		vlu = mathGiveup.get("three");
-        		mathGiveup.put("three", ++vlu);
+        	if(ans == one[count%one.length]) {
+        		vlu = mathGiveup.get(1);
+        		mathGiveup.put(1, ++vlu);
+        	}if(ans == two[count%two.length]) {
+        		vlu = mathGiveup.get(2);
+        		mathGiveup.put(2, ++vlu);
+        	}if(ans == three[count%three.length]) {
+        		vlu = mathGiveup.get(3);
+        		mathGiveup.put(3, ++vlu);
         	}
-        	count = (count == 10) ? 0 : ++count;
-        	if(count == answers.length) {
-//        		System.out.println(Collections.max(mathGiveup.keySet()));
-//        		for(String key : mathGiveup.keySet()) {
-//        			System.out.println(Math.max(mathGiveup.get(key), mathGiveup.get(key)));
-//        		}
-        		System.out.println(mathGiveup);
-        	}
+    		++count;
         }
+        
+//        max값을 뽑아 max값을 가지고 있는 유저들만 추출하는 알고리즘 검색 후 작성한 코드
+        int maxScore = Collections.max(mathGiveup.values());
+        
+        
+        for(int mathF : mathGiveup.keySet()) {
+        	if(maxScore == mathGiveup.get(mathF)) {
+        		list.add(mathF);
+        	}
+        	System.out.println(list);
+        }
+//        알고리즘 검색 전 코드
+//        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(mathGiveup.entrySet());
+//        entryList.sort(((o1, o2) -> o2.getValue() - o1.getValue()));
+        
+//        for(int i=0; i<entryList.size(); i++) {
+//        	System.out.println(entryList.get(i));
+//        	if(entryList.get(i).getValue() > entryList.get(i+1).getValue()) {
+//        		list.add(entryList.get(i).getKey());
+//        		break;
+//        	}if(entryList.get(i+1).getValue() == entryList.get(i+2).getValue()) {
+//        		list.add(entryList.get(i+1).getKey());
+//        		if(entryList.get(i+1).getValue() == entryList.get(i+2).getValue()) {
+//            		list.add(entryList.get(i+2).getKey());
+//            		break;
+//            	}
+//        		break;
+//        	}
+//        }
+        
+    	int[] answer = list.stream().mapToInt(i->i).toArray();
+    	
+    	Arrays.sort(answer);
+
         return answer;
     }
 }
