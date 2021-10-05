b40aedf3332b54cf83100928e5dafad0c17fc833
KimHeajin <goodcandy200@gmail.com>
2021-10-05 18:29:14 +0900

    프로그래머스-스택,큐/기능개발/100 프로그래머스-해시/위장/사용하지 않는 리스트 삭제 및 불필요한 print함수 제거

--- JavaSkillStudy/programmers/codeTest/developFacility.java
+++ JavaSkillStudy/programmers/codeTest/developFacility.java
@@ -3,7 +3,9 @@ package codeTest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,39 +14,62 @@ public class developFacility {
 	public static void main(String[] args) {
 		
 		int [] a = {95, 90, 99, 99, 80, 99};
+//		int [] a = {55,60,65};
 		int [] b = {1, 1, 1, 1, 1, 1};
+//		int [] b = {5, 10, 7};
 		System.out.println(solution(a, b ));
 	}
 	public static int[] solution(int[] progresses, int[] speeds) {
 		
-		ArrayList<Integer> progressesList = new ArrayList<Integer>(Arrays.stream(progresses).boxed().collect(Collectors.toList()));
-		ArrayList<Integer> speedsList = new ArrayList<Integer>(Arrays.stream(speeds).boxed().collect(Collectors.toList()));
-		ArrayList<Integer> saveList = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		ArrayList<Integer> countList = new ArrayList<Integer>();
-		int count = 1;
+		int count = 0;
+		int speedSum = 0;
 		
-        for(int i=0; i<=progressesList.size(); i++, count++) {
-        	if(i == progressesList.size()) {
-        		i=0;
-        	}
-        	progressesList.set(i, (progressesList.get(i) + speedsList.get(i)));
-        	if(progressesList.get(0)>=100) {
-    			for(int j=0; j<progressesList.size(); j++) {
-    				j=0;
-    				if(progressesList.get(j)>=100) {
-    					saveList.add(count);
-    					progressesList.remove(j);
-    					speedsList.remove(j);
-    				}
-        		}
-        	}
-        }
-        Set<Integer> set = new HashSet<Integer>(saveList);
-
-        for (int c : set) {
-        	countList.add(Collections.frequency(saveList, c));
-        }
-
+		for(int i=0; i<progresses.length; i++) {
+			speedSum = (100-progresses[i])%speeds[i] == 0 ? (100-progresses[i])/speeds[i] : ((100-progresses[i])/speeds[i])+1;
+			list.add(speedSum);
+		}
+		
+		for(int s=0; s<list.size(); s++) {
+			for(int j=s ; j<=list.size(); j++) {
+				if(j==list.size()) {
+					s=j;
+					break;
+				}else {
+					if(list.get(s)>=list.get(j)){
+						count++;
+					}else {
+						s=j-1;
+						break;
+					}
+				}
+			}
+			countList.add(count);
+			count=0;
+			//만약 앞에 값이 뒤의 값 보다 크다면? 뒤의 값도 저장해야함.
+//			if(list.get(s)>=list.get(s+1)) {
+//					// 현재의 값만큼 +를 해준다. 
+//					count++;
+//					// 이후 해당 값을 remove시킨다.
+////						list.remove(s);
+//					// 처음부터 다시 시작하게 한다.
+//			}else {
+//				// 만약 앞의 값이 뒤의값 보다 작다면? 현재의 값만 저장해야함.
+//				// 해당 else에 들어온 경우는 앞의 값보다 뒤의 값이 클 경우이다.
+//				// 이는 count를 +1해주던 작업이 끝났다는 것을 뜻하며, count해 준 값은 list에 넣어주어야 한다. 
+//				// 지금까지 count해주던 것을 countList에 넣어주었다.
+//				count++;
+//				countList.add(count);
+//				list.remove(s);
+//				// 처음부터 다시 시작하게 한다.
+//				// 그리고 count를 리셋해준다.
+//				count = 0;
+//			}
+			
+		}
+		
+		
         int[] answer = countList.stream().mapToInt(i->i).toArray();
 
         return answer;
--- JavaSkillStudy/programmers/codeTest/spyCamouflage.java
+++ JavaSkillStudy/programmers/codeTest/spyCamouflage.java
@@ -17,10 +17,8 @@ public class spyCamouflage {
 	public static int solution(String[][] clothes) {
         int answer = 1;
         
-//        Map<String, List<String>> spyClothes = new HashMap<>();
         // 숫자만 받을 때의 Map
         Map<String, Integer> spyClothes = new HashMap<>();
-        List<String> cl = new ArrayList<>();
         
         // 숫자만 받아올 때의 for문
         for(int i=0; i<clothes.length; i++) {
@@ -28,23 +26,10 @@ public class spyCamouflage {
         }
         
         for(String clothe : spyClothes.keySet()) {
-//        	System.out.println(clothe + "의 옷을 입은 경우 : " + spyClothes.get(clothe)  + ", 옷을 안입은 경우 : " + 1);
-//        	System.out.println(answer + "은 이전 옷. " + clothe + "종류의 옷을 입은 경우 : " + spyClothes.get(clothe)  + ", 그리고 옷을 안입은 경우인 " + 1+"을 합하면 " + (answer + spyClothes.get(clothe) + 1) +"이다.");
         	answer = answer * (spyClothes.get(clothe) + 1 );
-//        	System.out.println("합하고 곱한 결과 : " +answer);
         }
         // 아예 안입은 경우는 존재하지 않으므로 해당 경우를 -1해준다.
         answer--;
-        
-//        if(spyClothes.size()>1){
-//            for(String clothe : spyClothes.keySet()) {
-//            	
-//        		answer = answer * (spyClothes.get(clothe).size()+1);
-//
-//        		System.out.println(clothe + "의 식 : " + answer + " * " + spyClothes.get(clothe).size());
-//        	}
-//            answer--;
-//        }
         
         return answer;
     }
